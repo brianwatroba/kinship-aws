e504f5b3475b07cb3812a63bc5f3e7b6088d5006
brian watroba <brianwatroba@gmail.com>
2023-04-21 13:10:22 -0400

    dynamodb config setup, can get records

--- lambdas/receiveMessage.ts
+++ lambdas/receiveMessage.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { parseUrlEncoded } from '../utils/parseUrlEncoded';
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 
 export const receiveMessageHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
@@ -8,6 +9,21 @@ export const receiveMessageHandler = async (event: APIGatewayProxyEvent): Promis
         const { body } = event;
         if (body === null || body === undefined) throw new Error('No body in event');
         const { From: phoneNumber, Body: text } = parseUrlEncoded(body);
+
+        const client = new DynamoDBClient({ region: 'us-east-1' });
+
+        const params = {
+            TableName: 'Users',
+            KeyConditionExpression: 'phoneNumber = :phoneNumber',
+            ExpressionAttributeValues: {
+                ':phoneNumber': { S: 'phoneNumber' },
+            },
+        };
+        const command = new QueryCommand(params);
+
+        const data = await client.send(command);
+
+        console.log(data);
 
         // do something with the message
 
