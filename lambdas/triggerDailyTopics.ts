c4e9412d7006a521241bf341bc14c71e21343729
brian watroba <brianwatroba@gmail.com>
2023-05-01 07:44:10 -0400

    assigned prompts by day and level

--- lambdas/triggerDailyTopics.ts
+++ lambdas/triggerDailyTopics.ts
@@ -4,6 +4,7 @@ import { SQS_START_TOPIC_QUEUE_URL } from '../config/constants';
 import { Family } from '../models/Family';
 import { ScheduledEvent } from 'aws-lambda';
 import { prompts } from '../config/constants';
+import dayjs from 'dayjs';
 
 const getRandomValueInRange = (min: number, max: number): number => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,8 +13,30 @@ const getRandomValueInRange = (min: number, max: number): number => {
 export const triggerDailyTopicsHandler = async (event: ScheduledEvent): Promise<APIGatewayProxyResult> => {
     try {
         const allFamilies = await Family.scan().exec();
-        const randIndex = getRandomValueInRange(0, allFamilies.length - 1);
-        const prompt = prompts[randIndex];
+
+        const dayOfWeek = dayjs().day();
+        if (dayOfWeek === 2 || dayOfWeek === 4) {
+            // do nothing on Tuesday or Thursday
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: `Success!`,
+                }),
+            };
+        }
+        let promptsByDay;
+
+        if (dayOfWeek === 0) {
+            promptsByDay = prompts.heavy;
+        } else if (dayOfWeek === 1 || dayOfWeek === 3 || dayOfWeek === 5) {
+            promptsByDay = prompts.light;
+        } else {
+            promptsByDay = prompts.medium;
+        }
+
+        const randIndex = getRandomValueInRange(0, promptsByDay.length - 1);
+        if (promptsByDay === undefined) throw new Error('No prompts for today');
+        const prompt = promptsByDay[randIndex];
 
         const promises = allFamilies.map((family: Record<string, string>) => {
             const payload = {
